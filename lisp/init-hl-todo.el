cde636a21c0fa4f832ef8227ab31de9885c64f03
Fly_Lilee <liyingli2018@gmail.com>
2024-11-13 23:09:30 +0800

    add some todo hl keywords

--- lisp/init-hl-todo.el
+++ lisp/init-hl-todo.el
@@ -1,4 +1,3 @@
-(global-hl-todo-mode)
 (defun hl-todo-rg (regexp &optional files dir)
   "Use `rg' to find all TODO or similar keywords."
   (interactive
@@ -21,5 +20,20 @@
              (rg-read-files)
              (project-root (project-current))))))
   (rg regexp files dir))
+
+(setq hl-todo-keyword-faces
+      '(("TODO"   . "#FF0000")
+        ("todo"   . "#FF0000")
+        ("FIXME"  . "#FF0000")
+        ("fixme"  . "#FF0000")
+        ("DEBUG"  . "#A020F0")
+        ("debug"  . "#A020F0")
+        ("GOTCHA" . "#FF4500")
+        ("gotcha" . "#FF4500")
+        ("ANCHOR" . "#00ff00")
+        ("anchor" . "#00ff00")
+        ("STUB"   . "#1E90FF")
+        ("stub"   . "#1E90FF")))
+(global-hl-todo-mode)
 
 (provide 'init-hl-todo)
