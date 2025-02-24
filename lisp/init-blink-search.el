9ee9dda4db6af0eb66a27321a6b6a113e9a9b571
Fly_Lilee <liyingli2018@gmail.com>
2025-02-24 21:01:26 +0800

    feat: remove pdf source from blink-search backends

--- lisp/init-blink-search.el
+++ lisp/init-blink-search.el
@@ -2,8 +2,10 @@
 (setq blink-search-db-path no-littering-var-directory)
 (require 'blink-search)
 ;; (add-hook 'blink-search-mode-hook #'meow-insert-mode)
+;; (setq blink-search-search-backends
+;;       '("Buffer List" "Find File" "Common Directory" "Recent File" "Grep PDF" "PDF" "Google Suggest"))
 (setq blink-search-search-backends
-      '("Buffer List" "Find File" "Common Directory" "Recent File" "Grep PDF" "PDF" "Google Suggest"))
+      '("Buffer List" "Find File" "Common Directory" "Recent File" "Google Suggest"))
 
 (setq blink-search-common-directory
       '(("HOME"  "~/")
