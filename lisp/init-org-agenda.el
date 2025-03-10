184fed8c6be674efbc119d23b9a71b2b1ba0bfa5
Fly_Lilee <liyingli2018@gmail.com>
2025-03-10 13:50:12 +0800

    feat: add org-capture templates and show specific agenda entry rules

--- lisp/init-org-agenda.el
+++ lisp/init-org-agenda.el
@@ -14,6 +14,26 @@
 
 (add-list-to-list 'org-agenda-files (file-expand-wildcards (concat *org-path* "daily/*.org")))
 
+;; 计算待办事项创建至今的时间
+(defun org-todo-age (&optional pos)
+  "Calculate the age of an Org mode TODO entry.
+If POS is given, calculate the age of the TODO entry at that position.
+Otherwise, calculate the age of the current entry."
+  (if-let* ((entry-age (org-todo-age-time pos))
+            (days (time-to-number-of-days entry-age)))
+      (cond
+       ((< days 1)   "today")
+       ((< days 7)   (format "%dd" days))
+       ((< days 30)  (format "%.1fw" (/ days 7.0)))
+       ((< days 358) (format "%.1fM" (/ days 30.0)))
+       (t            (format "%.1fY" (/ days 365.0))))
+    ""))
+
+(defun org-todo-age-time (&optional pos)
+  (let ((stamp (org-entry-get (or pos (point)) "TIMESTAMP_IA" t)))
+    (when stamp
+      (time-subtract (current-time)
+                     (org-time-string-to-time stamp)))))
 
 (setq org-agenda-custom-commands
       '(("e" "Agenda, next actions and waiting"
@@ -21,7 +41,56 @@
                       (org-agenda-span 3)
                       (org-agenda-start-on-weekday nil)))
           (todo "TODO" ((org-agenda-overriding-header "Next Actions:")))
-          (todo "WAIT" ((org-agenda-overriding-header "Waiting:")))))))
+          (todo "WAIT" ((org-agenda-overriding-header "Waiting:")))))
+        ("N" "Notes" tags "NOTE"
+         ((org-agenda-overriding-header "Notes")
+          (org-tags-match-list-sublevels t)))
+        ("g" "GTD"
+         ((agenda "" nil)
+          (tags-todo "-inbox"
+                     ((org-agenda-overriding-header "Next Actions")
+                      (org-agenda-tags-todo-honor-ignore-options t)
+                      (org-agenda-todo-ignore-scheduled 'future)
+                      (org-agenda-skip-function
+                       (lambda ()
+                         (or (org-agenda-skip-subtree-if 'todo '("HOLD" "WAITING"))
+                            (org-agenda-skip-entry-if 'nottodo '("NEXT")))))
+                      (org-tags-match-list-sublevels t)
+                      (org-agenda-sorting-strategy
+                       '(todo-state-down effort-up category-keep))))
+          (tags-todo "-reading/PROJECT"
+                     ((org-agenda-overriding-header "Project")
+                      (org-agenda-prefix-format "%-11c%5(org-todo-age) ")
+                      (org-tags-match-list-sublevels t)
+                      (org-agenda-sorting-strategy
+                       '(category-keep))))
+          (tags-todo "+reading/PROJECT"
+                     ((org-agenda-overriding-header "Reading")
+                      (org-agenda-prefix-format "%-11c%5(org-todo-age) ")
+                      (org-tags-match-list-sublevels t)
+                      (org-agenda-sorting-strategy
+                       '(category-keep))))
+          (tags-todo "/WAITING"
+                     ((org-agenda-overriding-header "Waiting")
+                      (org-agenda-tags-todo-honor-ignore-options t)
+                      (org-agenda-todo-ignore-scheduled 'future)
+                      (org-agenda-sorting-strategy
+                       '(category-keep))))
+          (tags-todo "/DELEGATED"
+                     ((org-agenda-overriding-header "Delegated")
+                      (org-agenda-tags-todo-honor-ignore-options t)
+                      (org-agenda-todo-ignore-scheduled 'future)
+                      (org-agenda-sorting-strategy
+                       '(category-keep))))
+          (tags-todo "-inbox"
+                     ((org-agenda-overriding-header "On Hold")
+                      (org-agenda-skip-function
+                       (lambda ()
+                         (or (org-agenda-skip-subtree-if 'todo '("WAITING"))
+                            (org-agenda-skip-entry-if 'nottodo '("HOLD")))))
+                      (org-tags-match-list-sublevels nil)
+                      (org-agenda-sorting-strategy
+                       '(category-keep))))))))
 
 ;; Use a function to decide what to change the state to.
 (setq org-clock-in-switch-to-state #'sodaware/switch-task-on-clock-start)
@@ -37,15 +106,15 @@
 (setq org-agenda-scheduled-leaders '("Sched" "S.%2dx"))
 (setq org-agenda-deadline-leaders '("Deadl" "In%2dd" "D.%2dx"))
 
-(defun my/org-agenda-repeater ()
-  "The repeater shown in org-agenda-prefix for agenda."
-  (if (org-before-first-heading-p)
-      "-------"  ; fill the time grid
-    (format "%5s: " (or (org-get-repeat) ""))))
+;; (defun my/org-agenda-repeater ()
+;;   "The repeater shown in org-agenda-prefix for agenda."
+;;   (if (org-before-first-heading-p)
+;;       "-------"  ; fill the time grid
+;;     (format "%5s: " (or (org-get-repeat) ""))))
 
-;; Add `my/org-agenda-repeater' to the agenda prefix.
-(setcdr (assoc 'agenda org-agenda-prefix-format)
-        " %i %-12:c%?-12t%s%(my/org-agenda-repeater)")
+;; ;; Add `my/org-agenda-repeater' to the agenda prefix.
+;; (setcdr (assoc 'agenda org-agenda-prefix-format)
+;;         " %i %-12:c%?-12t%s%(my/org-agenda-repeater)")
 
 ;;; agennda format
 (setq org-agenda-span 'day
