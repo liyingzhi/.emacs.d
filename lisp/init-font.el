1548adbc13105c33a23cc0ce9b1794f20eeb08e5
Fly_Lilee <liyingli2018@gmail.com>
2025-03-12 09:38:00 +0800

    feat: add MonoLisa font

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -6,9 +6,8 @@
   "Setup fonts."
   (when (display-graphic-p)
     ;; Set default font
-    (cl-loop for font in '( "Jetbrains Mono" "Source Code Pro" "Cascadia Code" "Fira Code"
-                            "SF Mono" "Hack" "Menlo"
-                            "Monaco" "DejaVu Sans Mono" "Consolas")
+    (cl-loop for font in '( "Jetbrains Mono" "Source Code Pro" "MonoLisa Lucius" "Cascadia Code" "Fira Code"
+                            "SF Mono" "Hack" "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
              when (font-installed-p font)
              return (set-face-attribute 'default nil
                                         :family font
