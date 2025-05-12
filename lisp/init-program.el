109c65b74e1ac885e9c75dc41092f7b37b766c6f
Fly_Lilee <liyingli2018@gmail.com>
2025-05-12 10:12:11 +0800

    feat: remove xref search program

--- lisp/init-program.el
+++ lisp/init-program.el
@@ -22,7 +22,6 @@
 (global-projection-hook-mode)
 
 ;;; Xref
-(setq xref-search-program 'ripgrep)
 (setq xref-show-xrefs-function 'consult-xref)
 (setq xref-show-definitions-function 'consult-xref)
 
