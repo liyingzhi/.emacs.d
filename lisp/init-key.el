5f04ab22d867995923d584be75a23932c532e26d
Your Name <you@example.com>
2025-07-16 19:48:02 +0800

    feat: add org-insert-link org menu

--- lisp/init-key.el
+++ lisp/init-key.el
@@ -280,6 +280,7 @@
  (:key "c" :description "Open org capture" :command org-capture :filename "init-org")
  (:key "a" :description "Open org agenda" :command one-key-menu-agenda :filename "init-org")
  (:key "l" :description "Org store link" :command org-store-link :filename "init-org")
+ (:key "i" :description "Org insert link" :command org-insert-link :filename "init-org")
  (:key "s" :description "Org search" :command consult-notes)
  (:key "r" :description "Org roam" :command one-key-menu-roam)
  (:key "t" :description "Vterm" :command multi-vterm-open :filename "multi-vterm")
