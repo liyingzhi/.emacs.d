18282e17f58f5e9c87f59c99d4d84534aa2e489d
Fly_Lilee <liyingli2018@gmail.com>
2025-07-18 09:35:06 +0800

    feat: add find-custom-file keybinding

--- lisp/init-key.el
+++ lisp/init-key.el
@@ -124,7 +124,8 @@
    (("p" . "Find in project") . (lambda ()
                                   (interactive)
                                   (require 'project)
-                                  (ido-find-file-in-dir "~/MyProject")))))
+                                  (ido-find-file-in-dir "~/MyProject")))
+   (("c" . "Find custom file") . find-custom-file)))
 
 
 (defmacro open-dir (path)
