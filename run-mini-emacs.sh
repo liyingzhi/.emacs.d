ff40985939d650ee3217fd7a8bcf3b5ddb69fc23
Fly_Lilee <liyingli2018@gmail.com>
2025-08-24 00:08:08 +0800

    feat: add run mini-emacs start script and creat soft link

---
+++ run-mini-emacs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+emacs --no-site-file --init-directory="c:/EmacsConfig/Documents/github/mini-emacs/"
+
+# 创建 mini eamcs 软链接启动脚本
+# ln -s _path_/run-mini-emacs.sh ~/.local/bin/miniemacs
