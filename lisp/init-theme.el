b2f132f751f67f20a83587e46d267ed67e4483e3
Fly_Lilee <liyingli2018@gmail.com>
2025-09-11 09:28:43 +0800

    feat: refactor init-theme

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,58 +1,6 @@
-;; (use-package gruvbox-theme
-;;   :ensure t)
-
-;; (use-package doom-themes
-;;   :ensure t)
-
-;; (use-package monokai-theme
-;;   :ensure t)
-
-;; (use-package solarized-theme
-;;   :ensure t
-;;   :defer)
-
-;; (use-package modus-themes
-;;   :ensure t)
-
-;; (use-package ef-themes
-;;   :ensure t
-;;   :defer)
-
-;; (use-package flucui-themes
-;;   :ensure t
-;;   :defer)
-
-;; (load-theme 'dracula t)
-;; (load-theme 'doom-dracula t)
-;; (load-theme 'modus-vivendi t)
-;; (load-theme 'modus-operandi t)
-;; (load-theme 'doom-snazzy t)
-;; (load-theme 'doom-molokai t)
-;; (load-theme 'gruvbox-dark-soft t)
-;; (load-theme 'doom-one t)
-;; (load-theme 'tango-dark t)
-;; (load-theme 'monokai t)
-;; (load-theme 'solarized-dark t)
-;; (load-theme 'vscode-dark-plus t)
-;; (load-theme 'modus-vivendi t)
-;; (load-theme 'ef-summer t)
-;; (load-theme 'modus-operandi t)
-;; (load-theme 'ef-night t)
-;; (load-theme 'ef-day t)
-;; (load-theme 'ef-summer t)
-;; (flucui-themes-load-style 'dark)
-
-;; (use-package circadian
-;;   :ensure t
-;;   :config
-;;   ;; (setq calendar-latitude 37)
-;;   ;; (setq calendar-longitude 112)
-;;   ;; (setq circadian-themes '((:sunrise . ef-summer)
-;;   ;;                          (:sunset  . solarized-dark)))
-;;   (setq circadian-themes '(("8:00" . ef-day)
-;;                            ("16:00" . solarized-dark)))
-;;   (circadian-setup))
-
+;;; init-theme.el --- theme                          -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
 
 ;; 随机选择列表中的一个元素
 (defun a-random (list)
@@ -61,7 +9,9 @@
 
 ;; 随机加载一个白名单主题
 (defun a-random-theme (&optional use-light)
-  "Randomly load a whitelist theme, `t' for light"
+  "Randomly load a theme from the whitelist.
+When called interactively, press t to select a light theme.
+The USE-LIGHT argument specifies whether to use light themes."
   (interactive
    (list (eq (read-char "Press `t' for light theme:") ?t)))
   (let ((theme-pool (remove (car custom-enabled-themes)
@@ -91,3 +41,4 @@
   (catppuccin-reload))
 
 (provide 'init-theme)
+;;; init-theme.el ends here
