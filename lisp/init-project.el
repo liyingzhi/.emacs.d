610530b155571af1b153be876cdb88dd9b3ee7a2
Fly_Lilee <liyingli2018@gmail.com>
2025-09-15 11:10:00 +0800

    feat: projection type move language setting

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -37,20 +37,6 @@
   (let ((default-directory (project-root-path)))
     (blink-search)))
 
-;;; Projection Type
-(defvar projection-project-type-python-uv
-  (projection-type
-   :name 'python-uv
-   :predicate (defun projection-python-uv-project-p ()
-                (and (file-exists-p "pyproject.toml")
-                     (file-exists-p "uv.lock")))
-   :build "uv build"
-   :run (defun projection-python-uv-project-run-command ()
-          (concat "uv run " (file-truename (buffer-file-name))))))
-
-(add-to-list 'projection-project-types projection-project-type-python-uv)
-
-
 ;;; project-prefix-map
 ;; (defalias 'project-prefix-map project-prefix-map)
 
