03b64c70747cfce34d3207691e7f59b5a8d0b23e
Fly_Lilee <liyingli2018@gmail.com>
2025-09-22 10:38:06 +0800

    refactor: pangu

--- lisp/init-pangu.el
+++ lisp/init-pangu.el
@@ -1,3 +1,6 @@
+;;; init-pangu.el --- spacing between chinese and asicc char  -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
 
 (add-hooks '(markdown-mode org-mode)
            #'pangu-spacing-mode)
@@ -5,3 +8,4 @@
 (setq pangu-spacing-real-insert-separtor t)
 
 (provide 'init-pangu)
+;;; init-pangu.el ends here
