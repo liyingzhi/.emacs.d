896bb951d63ce36f2a7243c7ad128916cdc9256a
yl_lilee <liyingli2018@gmail.com>
2025-09-25 13:14:54 +0800

    feat: custom flyspell faces

--- lisp/init-spell.el
+++ lisp/init-spell.el
@@ -3,12 +3,28 @@
 ;;; Code:
 
 (require 'wucuo)
+
+;;; flyspell
+(set-face-attribute 'flyspell-incorrect nil
+                    :underline '(:style line :color "Pink")
+                    :foreground nil
+                    :background nil)
+
+(set-face-attribute 'flyspell-duplicate nil
+                    :underline '(:style line :color "DeepPink")
+                    :foreground nil
+                    :background nil)
+(keymap-global-set "C-M-$" #'ispell-word)
+
+;; (setq wucuo-font-faces-to-check '(font-lock-comment-face))
 ;; (setq wucuo-flyspell-start-mode "normal")
+
 (setq ispell-program-name "aspell")
 ;; You could add extra option "--camel-case" for camel case code spell checking if Aspell 0.60.8+ is installed
 ;; @see https://github.com/redguardtoo/emacs.d/issues/796
 (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together" "--run-together-limit=16"))
 
+;;; wucuo
 (setq wucuo-spell-check-buffer-predicate
       (lambda ()
         (not (memq major-mode
