5dc065be746d8be7e7b8b646060b83a6350f41f3
Fly_Lilee <liyingli2018@gmail.com>
2025-09-26 20:28:02 +0800

    fix: require immersive-trranslage

--- lisp/init-immersive-translate.el
+++ lisp/init-immersive-translate.el
@@ -2,6 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'immersive-translate)
+
 (setq immersive-translate-backend 'trans)
 (setq immersive-translate-trans-default-args
       (concat " " immersive-translate-trans-default-args))
