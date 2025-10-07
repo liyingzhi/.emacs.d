6d4748a433d18bee0a366aebd0b7d8887862a709
Fly_Lilee <liyingli2018@gmail.com>
2025-10-07 15:53:59 +0800

    feat: add hugo template and hugo default setting

--- lisp/init-hugo.el
+++ lisp/init-hugo.el
@@ -1,6 +1,8 @@
 (with-eval-after-load 'ox
   (require 'ox-hugo)
   (setq org-hugo-base-dir
-        (file-truename "~/MyProject/website/blog/")))
+        (file-truename "~/MyProject/website/blog/")
+        org-hugo-front-matter-format "yaml"
+        org-hugo-auto-set-lastmod t))
 
 (provide 'init-hugo)
