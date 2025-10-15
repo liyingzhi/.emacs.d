0df221e908616d2d8167795ef68b265d9944b108
Fly_Lilee <liyingli2018@gmail.com>
2025-10-15 11:21:45 +0800

    feat: disable org-count-words-mode for narrow capture buffer

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -228,6 +228,14 @@
   (when (buffer-file-name)
     (org-count-words-mode)))
 
+;; disable org-count-words-mode for capture narrow buffer
+(advice-add #'org-count-words-update-buffer-count
+            :before (lambda (_ &rest _)
+                      (when-let* ((name (buffer-name))
+                                  ((or (string-match-p "\\*Capture\\*" name)
+                                       (string-match-p "^CAPTURE-.*\\.org*" name))))
+                        (org-count-words-mode -1))))
+
 ;;; org rich yank
 (defun my-org-rich-yank-format-paste (language contents link)
   "Based on `org-rich-yank--format-paste-default'."
