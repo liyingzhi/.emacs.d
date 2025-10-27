c11ff33d36b444775011d98104395a38b03419a5
Fly_Lilee <liyingli2018@gmail.com>
2025-10-27 09:29:08 +0800

    feat: add port org agenda

--- lisp/init-org-agenda.el
+++ lisp/init-org-agenda.el
@@ -42,8 +42,140 @@ Otherwise, calculate the age of the current entry."
       (time-subtract (current-time)
                      (org-time-string-to-time stamp)))))
 
+(keymap-set org-agenda-mode-map
+            "C-c C-e" #'org-store-agenda-views)
+
+(defun sodaware/switch-task-on-clock-start (task-state)
+  "Change a task to 'IN-PROGRESS' when TASK-STATE is 'TODO'."
+  (if (string= task-state "TODO")
+      "DOING"
+    task-state))
+
+;; Use a function to decide what to change the state to.
+(setq org-clock-in-switch-to-state #'sodaware/switch-task-on-clock-start)
+
+
+;;; agenda repeater
+;; Shorten the leaders to reserve spaces for the repeater.
+(setq org-agenda-scheduled-leaders '("Sched" "S.%2dx"))
+(setq org-agenda-deadline-leaders '("Deadl" "In%2dd" "D.%2dx"))
+
+(defun my/org-agenda-repeater ()
+  "The repeater shown in org-agenda-prefix for agenda."
+  (if (org-before-first-heading-p)
+      "-------"  ; fill the time grid
+    (format "%5s: " (or (org-get-repeat) ""))))
+
+;; Add `my/org-agenda-repeater' to the agenda prefix.
+(setcdr (assoc 'agenda org-agenda-prefix-format)
+        " %i %-12:c%?-12t%s%(my/org-agenda-repeater)")
+
+;;; agennda format
+(setq org-agenda-span 'day
+      org-agenda-current-time-string (concat "◀┈┈┈┈┈┈┈┈┈┈┈┈┈ ⏰"))
+(setq org-agenda-compact-blocks t)
+
+(setq org-columns-default-format-for-agenda
+      "%TODO %PRIORITY(Pri) %60ITEM(Task) %SCHEDULED")
+
+(defun prot-org-agenda-include-priority-no-timestamp ()
+  "Return nil if heading has a priority but no timestamp.
+Otherwise, return the buffer position from where the search should
+continue, per `org-agenda-skip-function'."
+  (let ((point (point)))
+    (if (and (eq (nth 3 (org-heading-components)) ?A)
+             (not (org-get-deadline-time point))
+             (not (org-get-scheduled-time point)))
+        nil
+      (line-beginning-position 2))))
+
+(defvar prot-org-custom-daily-agenda
+  ;; NOTE 2021-12-08: Specifying a match like the following does not
+  ;; work.
+  ;;
+  ;; tags-todo "+PRIORITY=\"A\""
+  ;;
+  ;; So we match everything and then skip entries with
+  ;; `org-agenda-skip-function'.
+  `((tags-todo "*"
+               ((org-agenda-overriding-header "Important tasks without a date\n")
+                ;; NOTE 2024-10-31: Those used to work, but now the
+                ;; query for the timestamp is ignored.  I thus wrote
+                ;; `prot-org-agenda-include-priority-no-timestamp'.
+                ;;
+                ;; (org-agenda-skip-function '(org-agenda-skip-subtree-if nil '(timestamp)))
+                ;; (org-agenda-skip-function
+                ;;  `(org-agenda-skip-entry-if
+                ;;    'notregexp ,(format "\\[#%s\\]" (char-to-string org-priority-highest))))
+                (org-agenda-skip-function #'prot-org-agenda-include-priority-no-timestamp)
+                (org-agenda-block-separator nil)))
+    (agenda "" ((org-agenda-overriding-header "\nPending scheduled tasks")
+                (org-agenda-time-grid nil)
+                (org-agenda-start-on-weekday nil)
+                (org-agenda-span 1)
+                (org-agenda-show-all-dates nil)
+                (org-scheduled-past-days 365)
+                ;; Excludes today's scheduled items
+                (org-scheduled-delay-days 1)
+                (org-agenda-block-separator nil)
+                (org-agenda-entry-types '(:scheduled))
+                (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                (org-agenda-skip-function '(org-agenda-skip-entry-if 'regexp "ROUTINE"))
+                (org-agenda-day-face-function (lambda (date) 'org-agenda-date))
+                (org-agenda-format-date "")))
+    (agenda "" ((org-agenda-overriding-header "\nToday's agenda\n")
+                (org-agenda-span 1)
+                (org-deadline-warning-days 0)
+                (org-agenda-block-separator nil)
+                (org-scheduled-past-days 0)
+                (org-agenda-skip-function '(org-agenda-skip-entry-if 'regexp "ROUTINE"))
+                ;; We don't need the `org-agenda-date-today'
+                ;; highlight because that only has a practical
+                ;; utility in multi-day views.
+                (org-agenda-day-face-function (lambda (date) 'org-agenda-date))
+                (org-agenda-format-date "%A %-e %B %Y")))
+    ;; (agenda "" ((org-agenda-overriding-header "\nRoutine")
+    ;;             (org-agenda-time-grid nil)
+    ;;             (org-agenda-start-on-weekday nil)
+    ;;             (org-agenda-span 1)
+    ;;             (org-agenda-show-all-dates nil)
+    ;;             (org-scheduled-past-days 365)
+    ;;             ;; Excludes today's scheduled items
+    ;;             (org-scheduled-delay-days 1)
+    ;;             (org-agenda-block-separator nil)
+    ;;             (org-agenda-entry-types '(:scheduled))
+    ;;             (org-agenda-skip-function '(org-agenda-skip-entry-if 'notregexp "ROUTINE"))
+    ;;             (org-agenda-day-face-function (lambda (date) 'org-agenda-date))
+    ;;             (org-agenda-format-date "")))
+    (agenda "" ((org-agenda-overriding-header "\nNext three days\n")
+                (org-agenda-start-on-weekday nil)
+                (org-agenda-start-day nil)
+                (org-agenda-start-day "+1d")
+                (org-agenda-span 3)
+                (org-deadline-warning-days 0)
+                (org-agenda-block-separator nil)
+                (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))))
+    (agenda "" ((org-agenda-overriding-header "\nUpcoming deadlines (+14d)\n")
+                (org-agenda-time-grid nil)
+                (org-agenda-start-on-weekday nil)
+                ;; We don't want to replicate the previous section's
+                ;; three days, so we start counting from the day after.
+                (org-agenda-start-day "+4d")
+                (org-agenda-span 14)
+                (org-agenda-show-all-dates nil)
+                (org-deadline-warning-days 0)
+                (org-agenda-block-separator nil)
+                (org-agenda-entry-types '(:deadline))
+                (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done)))))
+  "Custom agenda for use in `org-agenda-custom-commands'.")
+
 (setq org-agenda-custom-commands
-      `(("e" "Agenda, next actions and waiting"
+      `(("A" "Daily agenda and top priority tasks"
+         ,prot-org-custom-daily-agenda
+         ((org-agenda-fontify-priorities nil)
+          (org-agenda-prefix-format "	 %t %s")
+          (org-agenda-dim-blocked-tasks nil)))
+        ("e" "Agenda, next actions and waiting"
          ((agenda "" ((org-agenda-overriding-header "Next three days:")
                       (org-agenda-span 3)
                       (org-agenda-start-on-weekday nil)))
@@ -61,7 +193,7 @@ Otherwise, calculate the age of the current entry."
                       (org-agenda-skip-function
                        (lambda ()
                          (or (org-agenda-skip-subtree-if 'todo '("HOLD" "WAITING"))
-                            (org-agenda-skip-entry-if 'nottodo '("NEXT")))))
+                             (org-agenda-skip-entry-if 'nottodo '("NEXT")))))
                       (org-tags-match-list-sublevels t)
                       (org-agenda-sorting-strategy
                        '(todo-state-down effort-up category-keep))))
@@ -94,47 +226,12 @@ Otherwise, calculate the age of the current entry."
                       (org-agenda-skip-function
                        (lambda ()
                          (or (org-agenda-skip-subtree-if 'todo '("WAITING"))
-                            (org-agenda-skip-entry-if 'nottodo '("HOLD")))))
+                             (org-agenda-skip-entry-if 'nottodo '("HOLD")))))
                       (org-tags-match-list-sublevels nil)
                       (org-agenda-sorting-strategy
                        '(category-keep)))))
          nil
          (,(concat user/org-base-dir-path "/emacs-agenda.html") ,(concat user/org-base-dir-path "/emacs-agenda.ps")))))
-
-(keymap-set org-agenda-mode-map
-            "C-c C-e" #'org-store-agenda-views)
-
-;; Use a function to decide what to change the state to.
-(setq org-clock-in-switch-to-state #'sodaware/switch-task-on-clock-start)
-
-(defun sodaware/switch-task-on-clock-start (task-state)
-  "Change a task to 'IN-PROGRESS' when TASK-STATE is 'TODO'."
-  (if (string= task-state "TODO")
-      "DOING"
-    task-state))
-
-;;; agenda repeater
-;; Shorten the leaders to reserve spaces for the repeater.
-(setq org-agenda-scheduled-leaders '("Sched" "S.%2dx"))
-(setq org-agenda-deadline-leaders '("Deadl" "In%2dd" "D.%2dx"))
-
-(defun my/org-agenda-repeater ()
-  "The repeater shown in org-agenda-prefix for agenda."
-  (if (org-before-first-heading-p)
-      "-------"  ; fill the time grid
-    (format "%5s: " (or (org-get-repeat) ""))))
-
-;; Add `my/org-agenda-repeater' to the agenda prefix.
-(setcdr (assoc 'agenda org-agenda-prefix-format)
-        " %i %-12:c%?-12t%s%(my/org-agenda-repeater)")
-
-;;; agennda format
-(setq org-agenda-span 'day
-      org-agenda-current-time-string (concat "◀┈┈┈┈┈┈┈┈┈┈┈┈┈ ⏰"))
-(setq org-agenda-compact-blocks t)
-
-(setq org-columns-default-format-for-agenda
-      "%TODO %PRIORITY(Pri) %60ITEM(Task) %SCHEDULED")
 
 ;;; agenda menu
 ;;;###autoload
@@ -148,7 +245,15 @@ Otherwise, calculate the age of the current entry."
  "agenda"
  `((("i" . "inbox agent") . ,(agenda-open-with-file "~/Documents/Org/inbox.org"))
    (("t" . "idea agent") . ,(agenda-open-with-file "~/Documents/Org/tasks.org"))
-   (("a" . "all agent") . org-agenda)
+   (("A" . "all agent") . org-agenda)
+   ("a" . (lambda ()
+            "Call Org agenda with `prot-org-custom-daily-agenda' configuration."
+            (interactive)
+            (org-agenda nil "A")))
+   ("g" . (lambda ()
+            "Call Org agenda with GTD configuration."
+            (interactive)
+            (org-agenda nil "g")))
    (("d" . "Capture roam dailies note") . org-roam-dailies-capture-today)
    (("j" . "Show open or delete journal menu") . journal-transient)
    (("p" . "casual timezone planner") . (lambda ()
