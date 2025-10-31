9f186a7f371b1f951d784d3ac33863f0a54705ef
Fly_Lilee <liyingli2018@gmail.com>
2025-10-31 09:28:09 +0800

    feat: add casual ediff

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -102,6 +102,11 @@ The completion candidates include the Git status of each file."
                 (with-current-buffer buffer
                   (outline-show-all))))))
 
+(casual-ediff-install)
+(add-hook 'ediff-keymap-setup-hook
+          (lambda ()
+            (keymap-set ediff-mode-map "C-o" #'casual-ediff-tmenu)))
+
 ;;; difftastic
 (require 'init-difftastic)
 
@@ -175,7 +180,8 @@ _p_rev       _U_pper              _=_: upper/lower       _r_esolve
    ["Git"
     ("b" "Switch Modified buffer" consult-switch-git-status-buffer)
     ("g" "Git Link" git-link-dispatch)
-    ("c" "Git Clone" ar/git-clone-clipboard-url)]]
+    ("c" "Git Clone" ar/git-clone-clipboard-url)
+    ("e" "Ediff revision" casual-ediff-revision)]]
   [("q" "Quit" transient-quit-one)])
 
 ;;; Local Variables
