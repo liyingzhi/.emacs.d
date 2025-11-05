44557817d2864df828430a51bbbe7fc50d072854
Fly_lilee <liyingli2018@gmail.com>
2025-11-05 22:42:24 +0800

    feat(org): Improve LaTeX export for Chinese documents
    Add custom LaTeX header with XeTeX and LXGW WenKai font for Chinese
    rendering. Update ctex package option to UTF8 encoding for better
    character support.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -51,7 +51,30 @@
    ("" "minted" nil)
    ;; for chinese preview
    ;; ("fontset=LXGW WenKai,UTF8" "ctex" t)
-   ("" "ctex" t)))
+   ("UTF8" "ctex" t))
+ org-format-latex-header "\\documentclass{article}
+\\usepackage[usenames]{color}
+\\usepackage{xeCJK}
+\\usepackage{amsmath}
+\\setCJKmainfont{LXGW WenKai}
+\\setmainfont{PragmataPro}
+\[DEFAULT-PACKAGES]
+\[PACKAGES]
+\\pagestyle{empty}             % do not remove
+% The settings below are copied from fullpage.sty
+\\setlength{\\textwidth}{\\paperwidth}
+\\addtolength{\\textwidth}{-3cm}
+\\setlength{\\oddsidemargin}{1.5cm}
+\\addtolength{\\oddsidemargin}{-2.54cm}
+\\setlength{\\evensidemargin}{\\oddsidemargin}
+\\setlength{\\textheight}{\\paperheight}
+\\addtolength{\\textheight}{-\\headheight}
+\\addtolength{\\textheight}{-\\headsep}
+\\addtolength{\\textheight}{-\\footskip}
+\\addtolength{\\textheight}{-3cm}
+\\setlength{\\topmargin}{1.5cm}
+\\addtolength{\\topmargin}{-2.54cm}"
+ )
 
 (add-hook 'org-mode-hook #'org-cdlatex-mode)
 
