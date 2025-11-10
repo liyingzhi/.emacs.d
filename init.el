a4fdf70daec649265b4257da354ca0506a263407
Fly_Lilee <liyingli2018@gmail.com>
2025-11-10 13:01:38 +0800

    feat: add axis

--- init.el
+++ init.el
@@ -57,6 +57,13 @@
 (require 'init-rsync)
 ;; (require 'init-code-stats)
 
+(require 'axis)
+(setq axis-db-location
+      (expand-file-name "var/axis-data.sqlite"
+                        user-emacs-directory))
+(add-hooks '(prog-mode text-mode)
+           #'axis-mode)
+
 (require 'init-ai)
 
 ;;; Programming
