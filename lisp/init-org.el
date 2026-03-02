264b3f8067a6c3798f7bc1abb8331f68d38e8729
Fly_lilee <liyingli2018@gmail.com>
2026-03-02 20:28:20 +0800

    feat(org): Add showall and inlineimages STARTUP shortkey
    Add "Pi" shortcut for showall inlineimages setup and rename "P" to
    "Pl" for latexpreview setup to avoid key conflict.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -470,8 +470,8 @@ prepended to the element after the #+HEADER: tag."
 
     ("L" (hot-expand "<L") "LaTeX")
     ("X" (yas-expand-snippet (yas-lookup-snippet "latex-chinese")) "Latex chinese")
-    ("P" (insert "#+STARTUP: latexpreview ") "Latex Preview")
-
+    ("pl" (insert "#+STARTUP: latexpreview ") "Latex Preview")
+    ("pi" (insert "#+STARTUP: showall inlineimages") "Showall and Image Preview")
     ("Mb" (insert "#+SETUPFILE: https://fniessen.github.io/org-html-themes/org/theme-bigblow.setup") "Html Bigblow Theme")
     ("Mr" (insert "#+SETUPFILE: https://fniessen.github.io/org-html-themes/org/theme-readtheorg.setup") "Html Readtheorg Theme")
     ("Mn" (insert "#+HTML_HEAD: <link rel=\"stylesheet\" type=\"text/css\" href=\"http://gongzhitaao.org/orgcss/org.css\"/>") "Html Normal Css"))
