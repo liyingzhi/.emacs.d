5a91a19ab338fdff12ab26ad69ba0045724ae143
Fly_lilee <liyingli2018@gmail.com>
2026-03-21 17:59:47 +0800

    feat(elfeed): add direct homepage and consult name filter for elfeed
    - Introduce `spike-leung/consult-elfeed` for selecting feeds with
    live preview
    - Add advice to open RSS links as site URLs in elfeed.org file
    - Define custom variable `user/elfeed-org-files` for centralized
    path configuration

--- lisp/init-elfeed.el
+++ lisp/init-elfeed.el
@@ -8,6 +8,8 @@
    elfeed-tube-mpv
    elfeed-org))
 
+(defconst user/elfeed-org-files "~/Documents/Org/elfeed.org"
+  "My elfeed org files path.")
 
 (with-eval-after-load 'elfeed
   ;; Load elfeed-org
@@ -21,7 +23,7 @@
   ;; Optionally specify a number of files containing elfeed
   ;; configuration. If not set then the location below is used.
   ;; Note: The customize interface is also supported.
-  (setopt rmh-elfeed-org-files (list "~/Documents/Org/elfeed.org"))
+  (setopt rmh-elfeed-org-files (list user/elfeed-org-files))
 
   (setopt url-queue-timeout 30)
 
@@ -29,6 +31,96 @@
 
   (require 'lib-elfeed)
   (setq elfeed-search-print-entry-function #'+elfeed-search-print-entry--better-default)
+
+  ;; Reference: https://taxodium.ink/use-elfeed-to-read-feed-in-emacs.html
+  (defun spike-leung/org-open-rss-feed-as-site-in-elfeed-org-files (orig-fun &rest args)
+    "Advice for `org-open-at-point' to redirect RSS links only in a specific file."
+    (let* ((element (org-element-context))
+           (link (and (eq (org-element-type element) 'link)
+                      (org-element-property :raw-link element))))
+      (if (and buffer-file-name
+               (string-equal (expand-file-name (buffer-file-name))
+                             (expand-file-name user/elfeed-org-files))
+               link
+               (string-match-p (rx (or "rss" "feed" "atom" "xml")) link))
+          (let* ((url-parts (url-generic-parse-url link))
+                 (scheme (url-type url-parts))
+                 (host (url-host url-parts))
+                 (site-url (concat scheme "://" host)))
+            (message "Opening site for feed: %s" site-url)
+            (browse-url site-url))
+        (apply orig-fun args))))
+
+  (advice-add 'org-open-at-point :around #'spike-leung/org-open-rss-feed-as-site-in-elfeed-org-files)
+
+
+  (defun spike-leung/get-feed-candidates (&optional level)
+    "Extract headings title from `rmh-elfeed-org-files' as consult candidates.
+If LEVEL exist, filter heading which level is greater or equal LEVEL."
+    (mapcan
+     (lambda (elfeed-org-file)
+       (with-current-buffer (or (find-buffer-visiting elfeed-org-file)
+                                (find-file-noselect elfeed-org-file))
+         (delq nil
+               (org-element-map (org-element-parse-buffer 'headline) 'headline
+                 (lambda (hl)
+                   ;; property 的值可以在这里找： https://orgmode.org/worg/dev/org-element-api.html
+                   (when (or (null level) (>= (org-element-property :level hl) level))
+                     (let* ((raw-title (org-element-property :raw-value hl))
+                            (title (org-link-display-format raw-title))
+                            (annotation (org-entry-get hl "description"))
+                            (feed-url (when (string-match org-link-bracket-re raw-title)
+                                        (match-string 1 raw-title))))
+                       (list :items (list title) :feed-url feed-url :annotation annotation))))
+                 nil))))
+     rmh-elfeed-org-files))
+
+  (defun spike-leung/elfeed-preview-state (state candidate)
+    "Return consult state function for live `elfeed' preview.
+See `consult--with-preview' about STATE and CANDIDATE."
+    (unless (null candidate)
+      (let* ((cand (car candidate))
+             (metadata (cdr candidate))
+             (feed-url (plist-get metadata :feed-url)))
+        (pcase state
+          ('setup
+           (unless (get-buffer "*elfeed-search*")
+             (elfeed-apply-hooks-now)
+             (elfeed-org)
+             (elfeed)
+             (elfeed-search-clear-filter))
+           (display-buffer "*elfeed-search*" '(display-buffer-reuse-window)))
+          ('preview
+           (elfeed-search-clear-filter)
+           (when (and cand (get-buffer "*elfeed-search*"))
+             (unless (string-empty-p cand)
+               (elfeed-search-set-filter (concat elfeed-search-filter " =" (string-replace " " "." cand))))))
+          ('return
+           (unless (string-empty-p cand)
+             (elfeed-search-set-filter (concat elfeed-search-filter " =" (string-replace " " "." cand)))
+             (elfeed-update-feed feed-url)))))))
+
+  (defun spike-leung/consult-elfeed ()
+    "Select feed from `rmh-elfeed-org-files' with live preview in `elfeed'."
+    (interactive)
+    (let* ((candidates (spike-leung/get-feed-candidates 3)))
+      (consult--multi candidates
+                      :prompt "Feed: "
+                      :state #'spike-leung/elfeed-preview-state
+                      :history 'spike-leung/consult-elfeed-history
+                      :annotate (lambda (cand)
+                                  (let* ((match-cand (seq-find
+                                                      (lambda (v)
+                                                        (string-match-p (car (plist-get v :items)) cand))
+                                                      candidates))
+                                         (annotation (and match-cand (plist-get match-cand :annotation))))
+                                    (when annotation
+                                      (concat (make-string 25 ?\s) annotation)))))
+      (when (get-buffer "*elfeed-search*")
+        (pop-to-buffer "*elfeed-search*"))))
+
+  (keymap-binds elfeed-search-mode-map
+    (("/") . spike-leung/consult-elfeed))
 
   (require 'elfeed-tube)
   (setopt elfeed-tube-backend 'yt-dlp)
