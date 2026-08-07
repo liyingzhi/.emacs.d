0f58e7bfa1e32dd4e1c23b1dd445a76383eebcb2
Fly_lilee <liyingli2018@gmail.com>
2026-08-07 14:45:15 +0800

    feat(completion): use crm-prompt on Emacs 31.1+
    The advice-based CRM indicator is retained for older Emacs versions.

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -2,16 +2,19 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Add prompt indicator to `completing-read-multiple'.
-;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-(defun crm-indicator (args)
-  (cons (format "[CRM%s] %s"
-                (replace-regexp-in-string
-                 "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-                 crm-separator)
-                (car args))
-        (cdr args)))
-(advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+(if (version< emacs-version "31.1")
+    (progn
+      ;; Add prompt indicator to `completing-read-multiple'.
+      ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+      (defun crm-indicator (args)
+        (cons (format "[CRM%s] %s"
+                      (replace-regexp-in-string
+                       "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                       crm-separator)
+                      (car args))
+              (cdr args)))
+      (advice-add #'completing-read-multiple :filter-args #'crm-indicator))
+  (setopt crm-prompt "[CRM%s] %p"))
 
 ;; Only list the commands of the current modes
 (when (boundp 'read-extended-command-predicate)
